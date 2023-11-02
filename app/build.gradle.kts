ad5f85fd7622d662ac39f09dbf4b4e752f2c63c8
VopRos-ZP <zolpavel366@gmail.com>
2023-11-02 21:11:33 +0300

    Добавлен `safe args`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.safeargs)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.android)
 }
